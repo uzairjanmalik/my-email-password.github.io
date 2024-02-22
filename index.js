8d50046a1ed3f1c1e2141a1c22f5aaab53ec81a5
uzairjanmalik <muhammaduzair1528@gmail.com>
2024-02-22 01:54:24 -0700

    check email and password

---
+++ index.js
@@ -0,0 +1,13 @@
+let  email = "suffah";
+var pwd = "123";
+email = prompt(`enter your email`);
+pwd = prompt(`enter password`);
+email == "suffah" && pwd == "123"? document.write("<center><h1>login successful</center></h1>"): document.write("<center><h1>invalid email or password</center></h1>")
+
+// var pwd = "123";
+// pwd = prompt(`enter password`);
+// pwd == "123"? document.write(`<h1><center>with correct password</h1></center>`):document.write(`<h1><center>with incorrect password</h1></center>`)
+
+
+
+
